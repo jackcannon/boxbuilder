ff720c19893e310e547aa4156916fe8d34d8a479
Jack Cannon <jackc@annon.co.uk>
2025-01-25 14:40:31 +0000

    Add logo to sidebar

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -4,6 +4,8 @@ import { exportSTL } from '../geometry/exportStl';
 import { FormObject, FormSchema } from '../form/schema';
 import { AutoForm } from '../autoform';
 
+import logo from '../../public/logo.svg';
+
 import './sidebar.css';
 
 interface Props {
@@ -16,6 +18,7 @@ interface Props {
 export const Sidebar = ({ style, form, setForm, solids }: Props) => {
   return (
     <section className="sidebar" style={style}>
+      <img src={logo} alt="logo" className="logo" />
       <AutoForm object={form} schema={FormSchema} onChange={setForm} />
       <nav>
         <button onClick={() => exportSTL(solids)}>Generate STL</button>
