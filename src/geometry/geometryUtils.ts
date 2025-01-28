52a538341ae00ffb2e941cd640afeb04b27e271d
Jack Cannon <jackc@annon.co.uk>
2025-01-28 23:04:15 +0000

    Fix preview rendering issue

--- src/geometry/geometryUtils.ts
+++ src/geometry/geometryUtils.ts
@@ -1,4 +1,10 @@
-import { NUM_SEGMENTS_MAX_EXPORT, NUM_SEGMENTS_MIN, TARGET_RESOLUTION_EXPORT, TARGET_RESOLUTION_PREVIEW } from '../constants';
+import {
+  NUM_SEGMENTS_MAX_EXPORT,
+  NUM_SEGMENTS_MAX_PREVIEW,
+  NUM_SEGMENTS_MIN,
+  TARGET_RESOLUTION_EXPORT,
+  TARGET_RESOLUTION_PREVIEW
+} from '../constants';
 
 export const limitRadius = (roundRadius: number, xSize: number, ySize: number): number => {
   let radius = roundRadius;
@@ -11,6 +17,8 @@ export const limitRadius = (roundRadius: number, xSize: number, ySize: number): 
 
 export const calculateSegments = (isPreview: boolean = false, radius: number): number => {
   const resolution = isPreview ? TARGET_RESOLUTION_PREVIEW : TARGET_RESOLUTION_EXPORT;
+  const minLimit = NUM_SEGMENTS_MIN;
+  const maxLimit = isPreview ? NUM_SEGMENTS_MAX_PREVIEW : NUM_SEGMENTS_MAX_EXPORT;
 
   const normalisedRadius = Math.max(0, Number(radius) || 0);
   const normalisedResolution = Math.max(0, Number(resolution) || 0);
@@ -20,7 +28,8 @@ export const calculateSegments = (isPreview: boolean = false, radius: number): n
   const circumference: number = 2 * Math.PI * normalisedRadius;
   const minSegments: number = Math.ceil(circumference / normalisedResolution);
 
-  let result = Math.max(NUM_SEGMENTS_MIN, Math.min(minSegments, NUM_SEGMENTS_MAX_EXPORT));
-  result = Math.ceil(result / 4) * 4; // round up to nearest 4
+  let result = Math.ceil(minSegments / 4) * 4; // round up to nearest 4
+  result = Math.max(minLimit, Math.min(result, maxLimit));
+
   return result;
 };
